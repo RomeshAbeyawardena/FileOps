48d9112f131ce0b0e6fc229a0973e98af1dc800a
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2024-01-27 12:51:36 +0000

    Refactor of files

---
+++ src/FileOps.Core/Features/Parse/Operations/OperationExecutorBase.cs
@@ -0,0 +1,20 @@
+﻿namespace FileOps.Core.Features.Parse.Operations;
+
+internal abstract class OperationExecutorBase : IOperationExecutor
+{
+    public OperationExecutorBase(Operation operation)
+    {
+        Operation = operation;
+    }
+
+    public Operation Operation { get; }
+
+    public virtual bool CanExecute(IOperationConfiguration configuration)
+    {
+        return configuration.Enabled 
+            && configuration.Operation == Operation;
+    }
+
+    public abstract Task Execute(IOperationConfiguration configuration, 
+        CancellationToken cancellationToken);
+}
---
+++ src/FileOps.Core/Features/Parse/Operations/OperationExecutorGenericBase.cs
@@ -0,0 +1,33 @@
+﻿namespace FileOps.Core.Features.Parse.Operations;
+
+internal abstract class OperationExecutorBase<TOperationConfiguration>
+    : OperationExecutorBase
+    where TOperationConfiguration : IOperationConfiguration
+{
+    public OperationExecutorBase(Operation operation) : base(operation) { }
+
+    public override bool CanExecute(IOperationConfiguration configuration)
+    {
+        return configuration is TOperationConfiguration operationConfiguration
+            && base.CanExecute(configuration) && this.CanExecute(configuration);
+    }
+
+    public override Task Execute(IOperationConfiguration configuration, 
+        CancellationToken cancellationToken)
+    {
+        if (configuration is TOperationConfiguration operationConfiguration)
+        {
+            return Execute(operationConfiguration, cancellationToken);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public virtual bool CanExecute(TOperationConfiguration configuration)
+    {
+        return true;
+    }
+
+    public abstract Task Execute(TOperationConfiguration configuration,
+        CancellationToken cancellationToken);
+}
