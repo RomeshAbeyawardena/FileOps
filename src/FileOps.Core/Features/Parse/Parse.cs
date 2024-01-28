b63e68a037f174cc8467e7fae8854f2a07848096
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2024-01-28 11:41:30 +0000

    Implemented Parse Command and Handler

---
+++ src/FileOps.Core/Features/Parse/Parse.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.Extensions.FileProviders;
+using System.Text.Json;
+
+namespace FileOps.Core.Features.Parse;
+
+internal class Parse(IFileProvider fileProvider) : IRequestHandler<ParseCommand, FileOpsConfiguration?>
+{
+    public async Task<FileOpsConfiguration?> Handle(ParseCommand request, CancellationToken cancellationToken)
+    {
+        var json = request.Json;
+        if (string.IsNullOrWhiteSpace(json) && !string.IsNullOrWhiteSpace(request.FileName))
+        {
+            var file = fileProvider.GetFileInfo(request.FileName);
+            if (!file.Exists)
+            {
+                throw new FileNotFoundException();
+            }
+
+            using var streamReader = new StreamReader(file.CreateReadStream());
+            json = await streamReader.ReadToEndAsync(cancellationToken);
+        }
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            throw new NullReferenceException("Unable to parse JSON from request or requested file");
+        }
+        using var jsonDocument = JsonDocument.Parse(json);
+
+        return JsonFileOpsConfiguration.Parse(jsonDocument);
+    }
+}
--- src/FileOps.Core/Features/Parse/ParseCommand.cs
+++ src/FileOps.Core/Features/Parse/ParseCommand.cs
@@ -2,7 +2,7 @@
 
 namespace FileOps.Core.Features.Parse;
 
-internal record ParseCommand : IRequest<FileOpsConfiguration>
+internal record ParseCommand : IRequest<FileOpsConfiguration?>
 {
     public string? FileName { get; set; }
     public string? Json { get; set; }
