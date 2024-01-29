ff292489f52eb908719d958c537e0f6decc9871d
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2024-01-29 14:52:03 +0000

    wip

--- src/FileOps.Core/Features/Process/IOperationProcessor.cs
+++ src/FileOps.Core/Features/Process/IOperationProcessor.cs
@@ -2,30 +2,6 @@
 
 namespace FileOps.Core.Features.Process;
 
-internal record OperationExecutorMapping
-{
-    public OperationExecutorMapping()
-    {
-        OperationConfiguration = Array.Empty<IOperationConfiguration>();
-    }
-
-    public IOperationExecutor? OperationExecutor { get; init; }
-    public IEnumerable<IOperationConfiguration> OperationConfiguration { get; init; }
-    public async Task ExecuteAll(OperationLedger operationLedger, 
-        CancellationToken cancellationToken)
-    {
-        foreach(var operationConfiguration in OperationConfiguration)
-        {
-            if (OperationExecutor != null)
-            {
-                OperationExecutor.LedgerEntries = operationLedger;
-                await OperationExecutor.Execute(operationConfiguration, cancellationToken);
-            }
-        }
-    }
-}
-
-
 internal interface IOperationProcessor
 {
     IEnumerable<OperationExecutorMapping> GetOperators(IFileOpsConfiguration configuration);
---
+++ src/FileOps.Core/Features/Process/OperationExecutorMapping.cs
@@ -0,0 +1,26 @@
+﻿using FileOps.Core.Operations;
+
+namespace FileOps.Core.Features.Process;
+
+internal record OperationExecutorMapping
+{
+    public OperationExecutorMapping()
+    {
+        OperationConfiguration = Array.Empty<IOperationConfiguration>();
+    }
+
+    public IOperationExecutor? OperationExecutor { get; init; }
+    public IEnumerable<IOperationConfiguration> OperationConfiguration { get; init; }
+    public async Task ExecuteAll(OperationLedger operationLedger, 
+        CancellationToken cancellationToken)
+    {
+        foreach(var operationConfiguration in OperationConfiguration)
+        {
+            if (OperationExecutor != null)
+            {
+                OperationExecutor.LedgerEntries = operationLedger;
+                await OperationExecutor.Execute(operationConfiguration, cancellationToken);
+            }
+        }
+    }
+}
