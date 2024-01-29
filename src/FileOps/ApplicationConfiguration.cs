8d83a5ba8d83a4c72a06a2117e6e115d4d6e5abd
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2024-01-29 23:08:21 +0000

    Added dependencies

---
+++ src/FileOps/ApplicationConfiguration.cs
@@ -0,0 +1,6 @@
+﻿namespace FileOps;
+
+internal class ApplicationConfiguration
+{
+    
+}
--- src/FileOps/Program.cs
+++ src/FileOps/Program.cs
@@ -1,15 +1,40 @@
-﻿using FileOps.Core;
+﻿using FileOps;
+using FileOps.Core;
 using FileOps.Core.Features.Parse;
 using FileOps.Core.Features.Process;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Console;
+using Shared.Console.Extensions;
 
 using var cancellationTokenRegistration = new CancellationTokenRegistration();
 var cancellationToken = cancellationTokenRegistration.Token;
 
-var serviceConfiguration = new ServiceCollection().RegisterServices(new[] { typeof(Program).Assembly });
+ApplicationConfiguration applicationConfiguration = new();
+applicationConfiguration.Parse(ArgumentParser.GetParameters(args));
+
+var serviceConfiguration = new ServiceCollection().RegisterServices(new[] {
+    typeof(Program).Assembly
+});
+
 var services = serviceConfiguration.BuildServiceProvider();
 var mediator = services.GetRequiredService<IMediator>();
-var parsedResult = await mediator.Send(new ParseCommand { }, cancellationToken);
 
-var processedResult = await mediator.Send(new ProcessCommand {  }, cancellationToken);
+var fileName = args.FirstOrDefault();
+if (!string.IsNullOrWhiteSpace(fileName))
+{
+    throw new NullReferenceException($"File '{fileName}' not found");
+}
+
+args.ElementAtOrDefault(1);
+
+
+
+var fileOpsConfiguration = await mediator.Send(new ParseCommand
+{
+    FileName = fileName
+}, cancellationToken);
+
+var processedResult = await mediator
+    .Send(new ProcessCommand { Configuration = fileOpsConfiguration },
+    cancellationToken);
